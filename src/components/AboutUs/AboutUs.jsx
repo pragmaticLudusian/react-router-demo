71b11a1a334be77c064cb2d1dd3ec3fcb5c1a3a5
pragmaticLudusian <arsen.r.m@gmail.com>
2025-01-20 02:00:30 +0200

    feat: add AmoutMe and AboutUs components and their nested link lists

--- src/components/AboutUs/AboutUs.jsx
+++ src/components/AboutUs/AboutUs.jsx
@@ -1,9 +1,19 @@
+import { Link, Outlet } from "react-router-dom";
 import "./AboutUs.css";
 
 function AboutUs() {
   return (
     <div className="about-us">
       <p>You can find out more information about our site here.</p>
+      <ul>
+        <li>
+          <Link to="site-mission">Site Mission</Link>
+        </li>
+        <li>
+          <Link to="site-history">Site History</Link>
+        </li>
+      </ul>
+      <Outlet />
     </div>
   );
 }
